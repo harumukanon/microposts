00f83cb57c7f13e8fdc82caa87476b64c2766952
harumukanon <mayizia8@gmail.com>
2015-12-15 14:16:38 +0000

    koshin dekitakamo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -12,16 +12,40 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      session[:user_id] = @user.id
+      render 'show'
     else
       render 'new'
     end
   end
   
+  def edit
+    if @user.id == session[:user_id]
+      render 'edit'
+    else
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はユーザ画面へ戻す
+      flash[:success] = "User information was revised."
+      render 'show'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :comment)
   end
 
 end
